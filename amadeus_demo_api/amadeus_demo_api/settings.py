0e6b31301ad6baad6cfb4562d1c4ed55a96dfe09
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-01-27 12:43:09 +0100

    add whitenosie to settings

--- amadeus_demo_api/amadeus_demo_api/settings.py
+++ amadeus_demo_api/amadeus_demo_api/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'amadeus_demo_api.urls'
