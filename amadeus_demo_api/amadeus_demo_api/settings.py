1d4cf6f3db29244ca6e96cf40cdd6d7aa7db334b
MinjiK <km.minjikim@gmail.com>
2022-03-23 16:54:03 +0100

    update allowed hosts

--- amadeus_demo_api/amadeus_demo_api/settings.py
+++ amadeus_demo_api/amadeus_demo_api/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'q77h6c^4l2fkcbfwnusbaas#()#mp48hj@_93o#h_+62u%%2t8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE', True)
-ALLOWED_HOSTS = ['amadeus4dev-book-flight.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['amadeus4dev-book-flight.herokuapp.com', 'localhost', '0.0.0.0', '127.0.0.1']
 
 # Application definition
 
