12e81733957a0084b6e709889227b15faab0ea5d
sohye0n <sbb0416@naver.com>
2025-05-09 21:36:05 +0900

    feat(auth): 로그인 페이지 추가 및 프론트/백엔드 연동 완료

--- amadeus_demo_api/amadeus_demo_api/settings.py
+++ amadeus_demo_api/amadeus_demo_api/settings.py
@@ -42,8 +42,7 @@ INSTALLED_APPS = [
     'chat',
     'users',
     'booking',
-    'chatbot',
-    'login'
+    'chatbot'
 ]
 
 MIDDLEWARE = [
--- amadeus_demo_api/amadeus_demo_api/urls.py
+++ amadeus_demo_api/amadeus_demo_api/urls.py
@@ -4,7 +4,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', include('chat.urls')),
-    path('login/', include('login.urls')),
+    path('login/', include('users.urls_front')),
+    
     path('', include('demo.urls')),
     path('api/', include('users.urls')),
     path('api/', include('chatbot.urls')),
